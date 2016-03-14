bf13e03146cd366adb1c12f022863e3d8b3664e5
XINGXIAOYU <14302010053@fudan.edu.cn>
2016-03-14 09:51:57 +0800

    test espresso

--- Lab1/src/lab1/EspressoTest.java
+++ Lab1/src/lab1/EspressoTest.java
@@ -1,5 +1,7 @@
 package lab1;
 
+import static org.junit.Assert.*;
+
 import java.text.DecimalFormat;
 
 import org.junit.Assert;
@@ -8,7 +10,8 @@ import org.junit.Test;
 
 import junit.framework.TestCase;
 
-public class DecafTest extends TestCase {
+public class EspressoTest extends TestCase {
+
 	Beverage order1;
 	Beverage order2;
 	Beverage order3;
@@ -21,52 +24,53 @@ public class DecafTest extends TestCase {
 	@Before
 	public void setUp() throws Exception {
 		order1 = new CoffeeBeverage();
-		order1 = new Decaf();
+		order1 = new Espresso();
 		order2 = new CoffeeBeverage();
-		order2 = new Decaf();
+		order2 = new Espresso();
 		order3 = new CoffeeBeverage();
-		order3 = new Decaf();
+		order3 = new Espresso();
 		order4 = new CoffeeBeverage();
-		order4 = new Decaf();
+		order4 = new Espresso();
 	}
 
 	@Test(timeout = 1000)
 	public void testDescription() {
-		// decaf
+		// espresso
 		result = order1.getDescription();
-		assertEquals("decaf", result);
-		// decaf+milk
+		assertEquals("Esspresso", result);
+		// espresso+milk
 		order2 = new Milk(order2);
 		result = order2.getDescription();
-		assertEquals("decaf milk", result);
-		// decaf+chocolate
+		assertEquals("Esspresso milk", result);
+		// espresso+chocolate
 		order3 = new Chocolate(order3);
 		result = order3.getDescription();
-		assertEquals("decaf chocolate", result);
-		// decaf+whipcream
+		assertEquals("Esspresso chocolate", result);
+		// espresso+whipcream
 		order4 = new WhipCream(order4);
 		result = order4.getDescription();
-		assertEquals("decaf whip", result);
+		assertEquals("Esspresso whip", result);
 	}
 
 	@Test(timeout = 1000)
 	public void testCost() {
+		// espresso small
 		DecimalFormat df = new DecimalFormat(".0");
-		// decaf small
 		((CoffeeBeverage) order1).setSize("small");
-		assertEquals(0.9, Double.parseDouble(df.format(order1.cost())));
-		// decaf medium milk
+		assertEquals(1.4, Double.parseDouble(df.format(order1.cost())));
+		// espresso medium milk
 		((CoffeeBeverage) order2).setSize("medium");
 		order2 = new Milk(order2);
-		assertEquals(1.5, Double.parseDouble(df.format(order2.cost())));
-		// decaf large chocolate
+		assertEquals(2.0, Double.parseDouble(df.format(order2.cost())));
+		// espresso large chocolate
 		((CoffeeBeverage) order3).setSize("large");
 		order3 = new Chocolate(order3);
-		assertEquals(1.8, Double.parseDouble(df.format(order3.cost())));
-		// decaf grand whipcream
+		assertEquals(2.3, Double.parseDouble(df.format(order3.cost())));
+		// espresso grand whipcream
 		((CoffeeBeverage) order4).setSize("grand");
 		order4 = new WhipCream(order4);
-		assertEquals(2.1, Double.parseDouble(df.format(order4.cost())));
+		order4.cost();
+		assertEquals(2.6, Double.parseDouble(df.format(order4.cost())));
 	}
 
 	@Test(timeout = 1000)
